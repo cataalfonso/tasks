e8bee6cc4fa721c713633bb3e0ab81a48bf54dc8
cata.alfonso <cata.alfonso@outlook.es>
2020-05-11 23:42:23 +0200

    trabajo semana 1

--- server/server.js
+++ server/server.js
@@ -2,11 +2,18 @@ var express = require('express');
 var path=require ('path');
 var app = express();
 
-app.get('/', function (req, res) {
+app.get('/', (req, res)=> {
   res.send('Hello World!');
 });
+
+
 app.use('/app', express.static('public'));
+
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 }); 
+
+app.post('/tasks', (req, res) => {
+  console.log('Hellooooooooooooooooo!')
+});
